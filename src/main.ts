20671b76e1359bd13d1ae505ba96611c2c08858e
Brito <eduardo@viewup.com>
2020-02-14 11:41:17 -0300

    Exporting Interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import IPChecker from './ipChecker';
+import { ISSHConnection, IIPChecker, IIPReport, ISSHConnectionParams } from '.';
 
 export default IPChecker;
+
+export { ISSHConnection, IIPChecker, IIPReport, ISSHConnectionParams };
